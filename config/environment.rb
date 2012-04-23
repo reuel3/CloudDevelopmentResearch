fe109c7783f4de8e1293a4b3acceece5c4807b95
reuel3@gmail.com <>
2012-04-23 18:25:41 +0000

    changed init sequence

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,12 @@
 Rails::Initializer.run do |config|    
   #additional or existing configuration
-  config.gem "asf-soap-adapter",&nbsp;:lib => 'asf-soap-adapter'   
+  config.gem "asf-soap-adapter",&nbsp;:lib => 'asf-soap-adapter'
+  # Initialize the rails application
+  OmniauthDemo::Application.initialize!
 end
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-OmniauthDemo::Application.initialize!
 
 # Set our instance URL for Force.com
 #ENV['sfdc_instance_url'] = 'https://na6.salesforce.com'
